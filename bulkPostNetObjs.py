d9dc39ce0f571eff4379f1779a36d6618e4e7e21
SD123456789 <47637207+SD123456789@users.noreply.github.com>
2020-09-21 11:26:11 -0400

    update netObjects

--- bulkPostNetObjs.py
+++ bulkPostNetObjs.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     # set needed variables to generate a token
     u = "apiUser"
     p = "Firepower~!"
-    ip = "ip.of.fmc"
+    ip = "ip.of.fmc:44327"
     path = "/api/fmc_platform/v1/auth/generatetoken"
     header = {} # don't need to instantiate this, but doing so for clarity
     payload = [] # don't need to instantiate this, but doing so for clarity
--- requestToken.py
+++ requestToken.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
     # set needed variables to generate a token
     u = "apiUser"
     p = "Firepower~!"
-    ip = "ip.of.fmc"
+    ip = "ip.of.fmc:44327"
     path = "/api/fmc_platform/v1/auth/generatetoken"
     header = {} # don't need to instantiate this here, but doing so for clarity
 
